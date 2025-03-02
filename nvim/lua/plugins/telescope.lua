7aba8dd8c133e741a2b4906c4cf8991dd03e1071
BabylooPro <babyloopro@gmail.com>
2025-03-02 15:07:43 +0100

    enhanced(telescope): ui with cursor theme, custom layouts, and detailed styling

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,83 +4,177 @@ return {
     branch = '0.1.x',
     dependencies = {
         'nvim-lua/plenary.nvim',
-        { -- If encountering errors, see telescope-fzf-native README for installation instructions
+        { -- IF ENCOUNTERING ERRORS, SEE TELESCOPE-FZF-NATIVE README FOR INSTALLATION INSTRUCTIONS
             'nvim-telescope/telescope-fzf-native.nvim',
 
-            -- `build` is used to run some command when the plugin is installed/updated.
-            -- This is only run then, not every time Neovim starts up.
+            -- `build` IS USED TO RUN SOME COMMAND WHEN THE PLUGIN IS INSTALLED/UPDATED.
+            -- THIS IS ONLY RUN THEN, NOT EVERY TIME NEOVIM STARTS UP.
             build = 'make',
 
-            -- `cond` is a condition used to determine whether this plugin should be
-            -- installed and loaded.
+            -- `cond` IS A CONDITION USED TO DETERMINE WHETHER THIS PLUGIN SHOULD BE
+            -- INSTALLED AND LOADED.
             cond = function()
                 return vim.fn.executable 'make' == 1
             end,
         },
         { 'nvim-telescope/telescope-ui-select.nvim' },
 
-        -- Useful for getting pretty icons, but requires a Nerd Font.
+        -- USEFUL FOR GETTING PRETTY ICONS, BUT REQUIRES A NERD FONT.
         { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     },
     config = function()
-        -- Telescope is a fuzzy finder that comes with a lot of different things that
-        -- it can fuzzy find! It's more than just a "file finder", it can search
-        -- many different aspects of Neovim, your workspace, LSP, and more!
-        --
-        -- The easiest way to use Telescope, is to start by doing something like:
-        --  :Telescope help_tags
-        --
-        -- After running this command, a window will open up and you're able to
-        -- type in the prompt window. You'll see a list of `help_tags` options and
-        -- a corresponding preview of the help.
-        --
-        -- Two important keymaps to use while in Telescope are:
-        --  - Insert mode: <c-/>
-        --  - Normal mode: ?
-        --
-        -- This opens a window that shows you all of the keymaps for the current
-        -- Telescope picker. This is really useful to discover what Telescope can
-        -- do as well as how to actually do it!
 
-        -- [[ Configure Telescope ]]
+        -- [[ CONFIGURE TELESCOPE ]]
         -- See `:help telescope` and `:help telescope.setup()`
-        require('telescope').setup {
-            -- You can put your default mappings / updates / etc. in here
-            --  All the info you're looking for is in `:help telescope.setup()`
+        local telescope = require('telescope')
+        local actions = require('telescope.actions')
+
+        telescope.setup {
             defaults = {
                 mappings = {
                     i = {
-                        ['<C-k>'] = require('telescope.actions').move_selection_previous, -- move to prev result
-                        ['<C-j>'] = require('telescope.actions').move_selection_next, -- move to next result
-                        ['<C-l>'] = require('telescope.actions').select_default, -- open file
+                        ['<C-k>'] = actions.move_selection_previous,
+                        ['<C-j>'] = actions.move_selection_next,
+                        ['<C-l>'] = actions.select_default,
                     },
+                },
+                prompt_prefix = "   ",
+                selection_caret = "  ",
+                entry_prefix = "  ",
+                initial_mode = "insert",
+                selection_strategy = "reset",
+                sorting_strategy = "ascending",
+                layout_strategy = "horizontal",
+                layout_config = {
+                    horizontal = {
+                        prompt_position = "top",
+                        preview_width = 0.55,
+                        results_width = 0.8,
+                    },
+                    vertical = {
+                        mirror = false,
+                    },
+                    width = 0.87,
+                    height = 0.80,
+                    preview_cutoff = 120,
+                },
+                border = true,
+                borderchars = {
+                    prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
+                    results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" },
+                    preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+                },
+                color_devicons = true,
+                set_env = { ["COLORTERM"] = "truecolor" },
+                file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+                grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+                qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+                buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+                vimgrep_arguments = {
+                    "rg",
+                    "--color=never",
+                    "--no-heading",
+                    "--with-filename",
+                    "--line-number",
+                    "--column",
+                    "--smart-case",
+                    "--hidden",
                 },
             },
             pickers = {
                 find_files = {
+                    theme = "cursor",
+                    previewer = false,
                     file_ignore_patterns = { 'node_modules', '.git', '.venv' },
                     hidden = true,
+                    results_title = false,
+                    prompt_title = false,
+                    layout_config = {
+                        width = 0.8,
+                        height = 0.6,
+                    },
                 },
-            },
-            live_grep = {
-                file_ignore_patterns = { 'node_modules', '.git', '.venv' },
-                additional_args = function(_)
-                    return { '--hidden' }
-                end,
+                buffers = {
+                    theme = "cursor",
+                    previewer = false,
+                    results_title = false,
+                    prompt_title = false,
+                    layout_config = {
+                        width = 0.8,
+                        height = 0.6,
+                    },
+                },
+                help_tags = {
+                    theme = "cursor",
+                    results_title = false,
+                    prompt_title = false,
+                    layout_config = {
+                        width = 0.8,
+                        height = 0.6,
+                    },
+                },
+                grep_string = {
+                    theme = "cursor",
+                    results_title = false,
+                    prompt_title = false,
+                    layout_config = {
+                        width = 0.8,
+                        height = 0.6,
+                    },
+                },
+                live_grep = {
+                    theme = "cursor",
+                    results_title = false,
+                    prompt_title = false,
+                    layout_config = {
+                        width = 0.8,
+                        height = 0.6,
+                    },
+                },
             },
             extensions = {
                 ['ui-select'] = {
-                    require('telescope.themes').get_dropdown(),
+                    require('telescope.themes').get_dropdown({
+                        borderchars = {
+                            { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+                            prompt = { "─", "│", " ", "│", "╭", "╮", "│", "│" },
+                            results = { "─", "│", "─", "│", "├", "┤", "╯", "╰" },
+                            preview = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+                        },
+                        width = 0.8,
+                        previewer = false,
+                        prompt_title = false,
+                        results_title = false,
+                    }),
+                },
+                fzf = {
+                    fuzzy = true,
+                    override_generic_sorter = true,
+                    override_file_sorter = true,
+                    case_mode = "smart_case",
                 },
             },
         }
 
-        -- Enable Telescope extensions if they are installed
+        -- ENABLE TELESCOPE EXTENSIONS IF THEY ARE INSTALLED
         pcall(require('telescope').load_extension, 'fzf')
         pcall(require('telescope').load_extension, 'ui-select')
 
-        -- See `:help telescope.builtin`
-        local builtin = require 'telescope.builtin'
+        -- OVERRIDE TELESCOPE HIGHLIGHTS TO MATCH OUR COLOR SCHEME
+        vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "#2a2a2a" })
+        vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#9ca0a4", bg = "#2a2a2a" })
+        vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "#2a2a2a" })
+        vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = "#9ca0a4", bg = "#2a2a2a" })
+        vim.api.nvim_set_hl(0, "TelescopePromptTitle", { fg = "#9ca0a4", bg = "#2a2a2a" })
+        vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { fg = "#9ca0a4", bg = "#2a2a2a" })
+        vim.api.nvim_set_hl(0, "TelescopeResultsTitle", { fg = "#9ca0a4", bg = "#2a2a2a" })
+        vim.api.nvim_set_hl(0, "TelescopeSelection", { bg = "#404040" })
+        vim.api.nvim_set_hl(0, "TelescopePreviewLine", { bg = "#404040" })
+        vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "#2a2a2a" })
+        vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = "#9ca0a4", bg = "#2a2a2a" })
+
+        -- KEYMAPS
+        local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
         vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
         vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
@@ -90,19 +184,19 @@ return {
         vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
         vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
         vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-        vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+        vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[S]earch [B]uffers' })
 
-        -- Slightly advanced example of overriding default behavior and theme
+        -- SLIGHTLY ADVANCED EXAMPLE OF OVERRIDING DEFAULT BEHAVIOR AND THEME
         vim.keymap.set('n', '<leader>/', function()
-            -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+            -- YOU CAN PASS ADDITIONAL CONFIGURATION TO TELESCOPE TO CHANGE THE THEME, LAYOUT, ETC.
             builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-                winblend = 10,
                 previewer = false,
+                theme = "dropdown",
             })
         end, { desc = '[/] Fuzzily search in current buffer' })
 
-        -- It's also possible to pass additional configuration options.
-        --  See `:help telescope.builtin.live_grep()` for information about particular keys
+        -- IT'S ALSO POSSIBLE TO PASS ADDITIONAL CONFIGURATION OPTIONS.
+        --  SEE `:help telescope.builtin.live_grep()` FOR INFORMATION ABOUT PARTICULAR KEYS
         vim.keymap.set('n', '<leader>s/', function()
             builtin.live_grep {
                 grep_open_files = true,
